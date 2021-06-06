a205f5d8903664a594b95b874233ef82c079d992
Simon Black <simon_black87@hotmail.co.uk>
2021-06-06 16:23:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num % 3 == 0
-    return "Fizz"
+     return "Fizz"
    elsif num % 5 == 0
-    return "Buzz"
+     return "Buzz"
+   elsif num % 3 == 0 && 5 == 0
+      return "FizzBuzz" 
+   else 
+     return
   end
 end
